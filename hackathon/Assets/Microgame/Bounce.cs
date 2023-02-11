a7f484de13493e81f7c09abb66472fd036168205
Tyler Morgan <tyler.morgan01@utrgv.edu>
2023-02-11 13:01:47 -0600

    initial commit

---
+++ hackathon/Assets/Microgame/Bounce.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float SPEED = 18.5f;
+    private Vector2 SCREEN_MAX;
+    private Vector2 SCREEN_MIN;
+    public Vector2 SELF_EXTENTS;
+    private Vector2 _velocity = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get screen size from camera (convert viewport 0,0 and 1,1 to world)
+        SCREEN_MAX = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        SCREEN_MIN = Camera.main.ViewportToWorldPoint(Vector2.zero);
+
+        // get self size from the sprite
+        // bounds.extents is the half width/height of the sprite in world units
+        SELF_EXTENTS = transform.GetComponent<SpriteRenderer>().sprite.bounds.extents;
+
+        // set starting velocity to random direction with magnitude SPEED
+        Vector2 dir = new Vector2(Random.Range(-1.0f, 1.0f),
+                                  Random.Range(-1.0f, 1.0f));
+        _velocity = dir.normalized * SPEED;
+    }
+
+    // public access to radius for collisions
+    public float GetRadius() { return SELF_EXTENTS.x; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // integrate velocity to update position
+        transform.position = transform.position + (Vector3)(_velocity * Time.deltaTime);
+
+        // detect wall collisions and respond
+        if (transform.position.x + SELF_EXTENTS.x > SCREEN_MAX.x) {
+            transform.position = (Vector3)new Vector2(SCREEN_MAX.x - SELF_EXTENTS.x, transform.position.y);
+            _velocity = (Vector3)new Vector2(-_velocity.x, _velocity.y);
+        }
+        else if (transform.position.x - SELF_EXTENTS.x < SCREEN_MIN.x) {
+            transform.position = (Vector3)new Vector2(SCREEN_MIN.x + SELF_EXTENTS.x, transform.position.y);
+            _velocity = (Vector3)new Vector2(-_velocity.x, _velocity.y);
+        }
+        if (transform.position.y + SELF_EXTENTS.y > SCREEN_MAX.y) {
+            transform.position = (Vector3)new Vector2(transform.position.x, SCREEN_MAX.y - SELF_EXTENTS.y);
+            _velocity = (Vector3)new Vector2(_velocity.x, -_velocity.y);
+        }
+        else if (transform.position.y - SELF_EXTENTS.y < SCREEN_MIN.y) {
+            transform.position = (Vector3)new Vector2(transform.position.x, SCREEN_MIN.y + SELF_EXTENTS.y);
+            _velocity = (Vector3)new Vector2(_velocity.x, -_velocity.y);
+        }
+
+    }
+}
---
+++ hackathon/Assets/Microgame/Spawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float _spawnTimer;
+    public const float SPAWN_INTERVAL = 5.0f;
+    private Vector2 SCREEN_MAX;
+    private Vector2 SCREEN_MIN;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get screen size from camera (convert viewport 0,0 and 1,1 to world)
+        SCREEN_MAX = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        SCREEN_MIN = Camera.main.ViewportToWorldPoint(Vector2.zero);
+
+        // start spawn timer and count down
+        _spawnTimer = SPAWN_INTERVAL;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // tick the timer down
+        _spawnTimer = _spawnTimer - Time.deltaTime;
+        if (_spawnTimer <= 0.0f) {
+            // timer expired, time to spawn if we still have children
+            if (transform.childCount >= 1) {
+                // unparent from this object
+                Transform child_transform = transform.GetChild(0);
+                child_transform.parent = null;
+
+                // set position (wall collision code will handle if it's too close to an edge)
+                child_transform.position = (Vector3)new Vector2(
+                    Random.Range(SCREEN_MIN.x, SCREEN_MAX.x),
+                    Random.Range(SCREEN_MIN.y, SCREEN_MAX.y));
+                    
+                // get the control script for that child and enabled, which will invoke Start/Update
+                GameObject child = child_transform.gameObject;
+                Bounce child_script = child.GetComponent<Bounce>();
+                child_script.enabled = true;
+            }
+
+            // reset timer (could deactivate self if children all gone)
+            _spawnTimer = SPAWN_INTERVAL;
+        }
+    }
+}
