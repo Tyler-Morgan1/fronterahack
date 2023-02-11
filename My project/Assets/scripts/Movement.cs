f862b1a604f100583d6b707ff4d77fe56135125f
Arturo Meza Canales <arturo.mezacanales01@utrgv.edu>
2023-02-11 15:12:17 -0600

    hey Art

--- My project/Assets/scripts/Movement.cs
+++ My project/Assets/scripts/Movement.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Movement : MonoBehaviour
 {
+        public Rigidbody2D ridge;
+        private Vector2 position;
+        private float speed = 15.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        position.x = Input.GetAxisRaw("Horizontal");
+
         
+    }
+
+    void FixedUpdate(){
+                ridge.MovePosition(ridge.position + position * speed * Time.fixedDeltaTime);
+
     }
 }
